32b9a69f3b35499be68b6c215d6aa6f44dacde60
Vivan <52017637+VivanM@users.noreply.github.com>
2020-11-01 22:00:49 -0700

    Update settings.py

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
